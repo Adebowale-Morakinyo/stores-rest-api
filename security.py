1fbc40a520d0fa687f6a8dfcd5d1c97f27cb206d
Adebowale Morakinyo <107467928+Adebowale-Morakinyo@users.noreply.github.com>
2022-08-24 17:05:54 -0700

    code deprecation

--- security.py
+++ security.py
@@ -1,10 +1,12 @@
-#from werkzeug.security import safe_str_cmp
-from models.user import UserModel
+from hmac import compare_digest
+from resources.user import UserModel
+
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if user and user.password == password:
+    if user and compare_digest(user.password, password):
         return user
+
 
 def identity(payload):
     user_id = payload['identity']
